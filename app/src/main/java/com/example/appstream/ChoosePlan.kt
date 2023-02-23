64fc79a60a9bc95143798f678fb4a14c6d7ee1ca
Pablo Alonso García - Afuera <pabalo172@alu.centroafuera.es>
2023-02-24 00:54:15 +0100

    Terminado

--- app/src/main/java/com/example/appstream/ChoosePlan.kt
+++ app/src/main/java/com/example/appstream/ChoosePlan.kt
@@ -1,11 +1,20 @@
 package com.example.appstream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appstream.databinding.ActivityChoosePlanBinding
 
 class ChoosePlan : AppCompatActivity() {
+    private lateinit var binding: ActivityChoosePlanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_plan)
+        binding = ActivityChoosePlanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView28.setOnClickListener(){
+            val intent = Intent(this, Terms::class.java)
+            startActivity(intent)
+        }
     }
 }
