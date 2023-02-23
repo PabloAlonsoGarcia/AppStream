8afc74cbe61a9cb84eeff312dc415c308458a794
Pablo Alonso García - Afuera <pabalo172@alu.centroafuera.es>
2023-02-23 13:03:22 +0100

    Main layout terminado Login 3.0 - Arreglado

--- app/src/main/java/com/example/appstream/LogIn.kt
+++ app/src/main/java/com/example/appstream/LogIn.kt
@@ -2,10 +2,13 @@ package com.example.appstream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appstream.databinding.ActivityLogInBinding
 
 class LogIn : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
